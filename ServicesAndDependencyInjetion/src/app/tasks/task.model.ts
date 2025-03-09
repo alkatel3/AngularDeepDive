7410ea4d218202d4e0d7f194d44f5a6229344911
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2025-03-09 19:35:14 +0200

    Made TaskStatusOptions exportable

--- ServicesAndDependencyInjetion/src/app/tasks/task.model.ts
+++ ServicesAndDependencyInjetion/src/app/tasks/task.model.ts
@@ -1,4 +1,37 @@
+import { InjectionToken, Provider } from "@angular/core";
+
 export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
+
+type TaskStatusOptions = {
+  value: 'open' | 'in-progress' | 'done',
+  textStatus: TaskStatus,
+  text: string
+}[]
+
+export const TASK_STATUS_OPTIONS = new InjectionToken<TaskStatusOptions>('task-status-options')
+
+export const TaskStatusOptions: TaskStatusOptions = [
+  {
+    value: 'open',
+    textStatus: 'OPEN',
+    text: 'Open'
+  },
+  {
+    value: 'in-progress',
+    textStatus: 'IN_PROGRESS',
+    text: 'In-Progres'
+  },
+  {
+    value: 'done',
+    textStatus: 'DONE',
+    text: 'Completed'
+  },
+]
+
+export const taskStatusOptionsProvider: Provider = {
+  provide: TASK_STATUS_OPTIONS,
+  useValue: TaskStatusOptions
+}
 
 export interface Task {
   id: string;
--- ServicesAndDependencyInjetion/src/app/tasks/tasks.service.ts
+++ ServicesAndDependencyInjetion/src/app/tasks/tasks.service.ts
@@ -1,12 +1,14 @@
-import { Injectable, signal } from "@angular/core";
+import { inject, Injectable, signal } from "@angular/core";
 import { Task, TaskStatus } from "./task.model";
+import { LoggingService } from "../logging.service";
 
-@Injectable({
-    providedIn: 'root',
+// @Injectable({
+//     providedIn: 'root',
 
-})
+// })
 export class TasksService {
     private tasks = signal<Task[]>([]);
+    private loggingService = inject(LoggingService)
 
     allTasks = this.tasks.asReadonly();
 
@@ -17,9 +19,11 @@ export class TasksService {
             status: 'OPEN'
         }
         this.tasks.update((oldTask) => [...oldTask, newTask])
+        this.loggingService.log('ADDED TASK WITH TITLE' + taskData.title)
     }
 
     updateTaskStatus(tastId: string, newStatus: TaskStatus) {
         this.tasks.update((oldTasks) => oldTasks.map((task) => task.id === tastId ? { ...task, status: newStatus } : task))
+        this.loggingService.log('CHANGES TASK STATUS TO' + newStatus)
     }
 }
