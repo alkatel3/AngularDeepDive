16fdcb7791400b3a1f157a3e55ea1f17b1bac655
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2025-03-11 22:39:41 +0200

    Made observable signal

--- ObservableInAngular/src/app/app.component.ts
+++ ObservableInAngular/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 import { interval, map } from 'rxjs';
 
@@ -8,18 +9,36 @@ import { interval, map } from 'rxjs';
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  private destroyRef = inject(DestroyRef)
+  clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount)
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    // effect(() => {
+    //   console.log(`Clicked button ${this.clickCount()} times`)
+    // })
+    toObservable(this.clickCount)
+  }
 
   ngOnInit(): void {
-    const subscription = interval(1000).pipe(
-      map((val) => val * 2)
-    ).subscribe({
-      next: (val) => console.log(val)
-    });
+    // const subscription = interval(1000).pipe(
+    //   map((val) => val * 2)
+    // ).subscribe({
+    //   next: (val) => console.log(val)
+    // });
 
-    this.destroyRef.onDestroy(() => {
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe()
+    // })
+    const subscription = this.clickCount$.subscribe({
+      next: (val) =>console.log(val) 
+    })
+      this.destroyRef.onDestroy(() => {
       subscription.unsubscribe()
     })
   }
 
+  onClick() {
+    this.clickCount.update(prevCount => prevCount + 1)
+  }
 }
