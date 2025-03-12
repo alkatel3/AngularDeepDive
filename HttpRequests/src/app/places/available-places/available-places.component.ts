51a9987523e8a5924db4690c6760c6b8a30a437e
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2025-03-12 22:17:36 +0200

    Added error handling

--- HttpRequests/src/app/places/available-places/available-places.component.ts
+++ HttpRequests/src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -15,19 +15,33 @@ import { map } from 'rxjs';
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('')
   private httpCliet = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
-    const subscription = this.httpCliet.get<{places: Place[]}>('http://localhost:3000/places')
-    .pipe(
-      map((resData)=>resData.places)
-    )
-    .subscribe({
-      next: (places) => {
-        this.places.set(places);
-      }
-    });
+    this.isFetching.set(true);
+    const subscription = this.httpCliet.get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(() => new Error('Something went wrong fetching the available places. Please try again later.'))
+        })
+      )
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+        },
+        error: (error: Error) =>{
+          console.log(error.message);
+          this.error.set(error.message);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        }
+      });
 
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
