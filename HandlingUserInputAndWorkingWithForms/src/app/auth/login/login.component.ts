efdf1867a6d310af255571bc554ecafb91220b2a
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2025-03-14 22:07:33 +0200

    Implemented form recovering

--- HandlingUserInputAndWorkingWithForms/src/app/auth/login/login.component.ts
+++ HandlingUserInputAndWorkingWithForms/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime, timeout } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +10,32 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('form')
+  private destroyRef = inject(DestroyRef)
+
+  constructor() {
+    afterNextRender(() => {
+      const savedForm = window.localStorage.getItem('saved-login-form')
+
+      if (savedForm) {
+        setTimeout(() => {
+          this.form().setValue({
+            email: JSON.parse(savedForm).email,
+            password: ''
+          })
+        }, 1)
+      }
+
+      var subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
+        next: (value) => window.localStorage.setItem('saved-login-form', JSON.stringify({ email: value.email }))
+      })
+
+      this.destroyRef.onDestroy(() => {
+        subscription?.unsubscribe()
+      })
+    })
+  }
+
   onSubmit(formData: NgForm) {
     if (formData.form.invalid) {
       return;
@@ -17,6 +44,8 @@ export class LoginComponent {
     const enteredEmail = formData.form.value.email;
     const entersPassword = formData.form.value.password;
     console.log(enteredEmail, entersPassword);
+
+    formData.form.reset()
 
   }
 }
