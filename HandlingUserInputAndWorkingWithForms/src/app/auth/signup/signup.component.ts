6b21701c9b1e206ca5d87e7629aa39b6c748d43b
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2025-03-15 22:43:05 +0200

    Added confirm password logic

--- HandlingUserInputAndWorkingWithForms/src/app/auth/signup/signup.component.ts
+++ HandlingUserInputAndWorkingWithForms/src/app/auth/signup/signup.component.ts
@@ -1,5 +1,20 @@
 import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+function equalValues(controlName1: string,  controlName2: string) {
+
+  return (control: AbstractControl) => {
+    const val1 = control.get(controlName1)?.value;
+    const val2 = control.get(controlName2)?.value;
+
+    if (val1 === val2) {
+      return null;
+    }
+
+    return { valuesNotEqual: true }
+  }
+
+}
 
 @Component({
   selector: 'app-signup',
@@ -20,6 +35,8 @@ export class SignupComponent {
       confirmPassword: new FormControl('', {
         validators: [Validators.minLength(6), Validators.required]
       }),
+    }, {
+      validators: [equalValues('password', 'confirmPassword')]
     }),
     firstName: new FormControl('', {
       validators: [Validators.required]
@@ -55,9 +72,9 @@ export class SignupComponent {
   })
 
   onSubmit() {
-    if(this.form.invalid){
+    if (this.form.invalid) {
       console.log("INVALID FROM");
-      return;      
+      return;
     }
 
     console.log(this.form);
