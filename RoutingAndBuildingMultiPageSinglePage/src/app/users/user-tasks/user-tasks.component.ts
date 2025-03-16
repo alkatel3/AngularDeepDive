97c7771fcabde48ab406149565592ad585c19c19
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2025-03-16 14:51:31 +0200

    Displayed user tasks with routing

--- RoutingAndBuildingMultiPageSinglePage/src/app/users/user-tasks/user-tasks.component.ts
+++ RoutingAndBuildingMultiPageSinglePage/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent implements OnInit {
+  // userId = input.required<string>();
+  userName = ''
+  private usersService = inject(UsersService);
+  private activateRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+
+  // userName = computed(() => this.usersService.users.find(u => u.id === this.userId())?.name)
+
+  ngOnInit() {
+    console.log(this.activateRoute)
+    const subscription = this.activateRoute.paramMap.subscribe({
+      next: paramMap => {
+        this.userName = this.usersService.users.find(u => u.id === paramMap.get('userId'))?.name || '';
+      }
+    })
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
+}
