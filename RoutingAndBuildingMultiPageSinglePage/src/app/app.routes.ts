dd1546d488e1a439f1e2711ac240436268d57716
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2025-03-17 23:00:44 +0200

    Implemented tasks sort with query param

--- RoutingAndBuildingMultiPageSinglePage/src/app/app.routes.ts
+++ RoutingAndBuildingMultiPageSinglePage/src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from "@angular/router";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import {routes as userRountes } from "./users/users.routes"
 import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const routes: Routes = [
@@ -13,21 +12,7 @@ export const routes: Routes = [
     {
         path: 'users/:userId',
         component: UserTasksComponent,
-        children: [
-            {
-                path: '',
-                redirectTo: 'tasks',
-                pathMatch: 'prefix'
-            },
-            {
-                path: 'tasks',
-                component: TasksComponent
-            },
-            {
-                path: 'tasks/new',
-                component: NewTaskComponent
-            },
-        ]
+        children: userRountes
     },
     {
         path: '**',
--- RoutingAndBuildingMultiPageSinglePage/src/app/tasks/tasks.component.ts
+++ RoutingAndBuildingMultiPageSinglePage/src/app/tasks/tasks.component.ts
@@ -1,19 +1,45 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, OnInit, signal } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent {
-  userId = input.required<string>()
+export class TasksComponent implements OnInit {
+  userId = input.required<string>();
+  order = signal<'asc' | 'desc'>('desc');
   private taskService = inject(TasksService);
-  
-  userTasks = computed(() => this.taskService.allTasks().filter(task => task.userId === this.userId()))
+
+  userTasks = computed(() =>
+    this.taskService
+      .allTasks()
+      .filter(task => task.userId === this.userId())
+      .sort((a,b)=>{
+        if(this.order()==='desc'){
+          return a.id > b.id ? -1 : 1
+        }
+
+        return a.id < b.id ? -1 : 1
+      })
+)
+
+  private activateRoute = inject(ActivatedRoute)
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    const subscription = this.activateRoute.queryParams.subscribe({
+      next: params => this.order.set(params['order'])
+    })
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
 }
