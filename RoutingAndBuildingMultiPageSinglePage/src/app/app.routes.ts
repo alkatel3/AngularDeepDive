c68dd7bc72bdc65b5f50ffecbe75c6ac23a45b9a
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2025-03-18 22:10:12 +0200

    Added example of using canMatch

--- RoutingAndBuildingMultiPageSinglePage/src/app/app.routes.ts
+++ RoutingAndBuildingMultiPageSinglePage/src/app/app.routes.ts
@@ -1,8 +1,18 @@
-import { Routes } from "@angular/router";
+import { CanMatchFn, RedirectCommand, Router, Routes } from "@angular/router";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { resolveTitle, resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { routes as userRountes } from "./users/users.routes"
 import { NotFoundComponent } from "./not-found/not-found.component";
+import { inject } from "@angular/core";
+
+const dummycanMatch: CanMatchFn = (route, segments) => {
+    const router = inject(Router)
+    const shouldGetAccess = Math.random();
+    if (shouldGetAccess < 0.5) {
+        return true;
+    }
+    return new RedirectCommand(router.parseUrl('/unauthorized'));
+}
 
 export const routes: Routes = [
     {
@@ -14,6 +24,7 @@ export const routes: Routes = [
         path: 'users/:userId',
         component: UserTasksComponent,
         children: userRountes,
+        canMatch: [dummycanMatch],
         data: {
             message: 'Hello!'
         },
